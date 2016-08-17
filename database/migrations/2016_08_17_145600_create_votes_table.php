babe9941ab557341534ff919e7817cf01eee6693
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-08-17 11:51:55 -0500

    "made migration files"

--- database/migrations/2016_08_17_145600_create_votes_table.php
+++ database/migrations/2016_08_17_145600_create_votes_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateVotesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,15 +12,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) 
+        Schema::create('votes', function (Blueprint $table) 
         {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+            $table->integer('user_id');
+            $table->integer('post_id');
+            $table->integer('votes');
             $table->timestamps();
         });
+
     }
 
     /**
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('votes');
     }
 }
