6e9d2249178b2880d1f241a7a3b8e31b5fa12ea2
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-08-18 15:04:34 -0500

    "dates laravel exercise"

--- app/Post.php
+++ app/Post.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $table = 'posts';
+    //dont really need this line laravel will use these col names automagically
+    protected $dates = ['created_at', 'updated_at'];
 
     public static $rules = [
         'title' => 'required|max:100',
