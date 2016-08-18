ce8d49f4e529786a2ad8c7d8dfb5ecfe6d5ae5b6
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-08-18 14:41:00 -0500

    "finished everything up to pagination exercise"

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,90 +9,100 @@ use App\Http\Controllers\Controller;
 
 class PostsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $posts = \App\Post::all();
-        // dd($posts);
-        return view('posts.index', ['posts' => $posts]);
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		$posts = \App\Post::paginate(4);
+		return view('posts.index', ['posts' => $posts]);
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('posts.create');
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		return view('posts.create');
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $post = new  \App\Post();
-        $post->title = $request->input('title');
-        $post->url= $request->input('url');
-        $post->content  = $request->input('content');
-        $post->user_id = 1;
-        $post->save();
-        return redirect()->action('PostsController@index');
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		
+    	$this->validate($request, \App\Post::$rules);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $post = \App\Post::find($id);
-        // dd($data);
-        return view('posts.show', ['post' => $post]);
-    }
+		$post = new  \App\Post();
+		$post->title = $request->input('title');
+		$post->url= $request->input('url');
+		$post->content  = $request->input('content');
+		$post->user_id = 1;
+		$post->save();
+		return redirect()->action('PostsController@index');
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        return 'You can edit post on this page';
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		$post = \App\Post::find($id);
+		// dd($data);
+		return view('posts.show', ['post' => $post]);
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        return 'Update a specific post';
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{   
+		$post = \App\Post::find($id);
+		return view('posts.edit', ['post' => $post]);
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $post = \App\Post::find($id);
-        $post->delete();
-    }
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		$this->validate($request, \App\Post::$rules);
+
+		$post = \App\Post::find($id);
+		$post->title = $request->input('title');
+		$post->url= $request->input('url');
+		$post->content  = $request->input('content');
+		$post->save();
+		return redirect()->action('PostsController@index');
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		$post = \App\Post::find($id);
+		$post->delete();
+	}
 }
--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $table = 'posts';
+
+    public static $rules = [
+        'title' => 'required|max:100',
+        'url'   => 'required',
+        'content' => 'required'
+    	];
+
+    protected function formatValidationErrors(Validator $validator)
+    {
+        return $validator->errors()->all();
+    }
+
 }
